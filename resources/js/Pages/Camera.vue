5703e1d59f758dd919f61da10272fc85bf903a09
Dhimas Ardinata <dhimasardinatapp@gmail.com>
2026-02-13 14:38:57 +0700

    chore: revert fan naming to drum fan

--- resources/js/Pages/Camera.vue
+++ resources/js/Pages/Camera.vue
@@ -36,7 +36,7 @@ const actuators = ref([
         status: false,
     },
     {
-        name: "Circulation Fan",
+        name: "Drum Fan",
         icon: "fas fa-fan",
         color: "text-red-500",
         status: false,
