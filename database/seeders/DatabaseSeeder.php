b767483f69d9e56500679d21d4db8fa9cd88b04d
Dhimas Ardinata <dhimasardinatapp@gmail.com>
2026-02-25 19:09:54 +0700

    chore: include RSSI seeder in default database seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             GreenhouseSeeder::class,
             SensorSeeder::class,
+            SensorRSSISeeder::class,
         ]);
     }
 }
