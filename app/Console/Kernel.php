e405f6cebb350a7084ef0aa02c132972ba236f8d
Saka284 <saka31.sw@gmail.com>
2026-03-27 09:31:40 +0700

    fix notification endpoint 3

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
+        $schedule->command('check:fog-notification')->everyMinute();
         // $schedule->command('inspire')->hourly();
     }
 
